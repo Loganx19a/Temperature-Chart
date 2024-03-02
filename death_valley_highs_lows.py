d95c79833d78a8369d0719657fb350ce5ea5bdde
Logan <Loganx19a@ksu.edu>
2024-03-02 02:36:06 -0600

    daily highs/lows (2021 Death Valley, CA) + automatic indexes and error handling

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -4,22 +4,38 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-path = Path('./weather_data/sitka_weather_2021_simple.csv')  # build a Path object, passing it the path to the weather data file we want
+path = Path('./weather_data/death_valley_2021_simple.csv')  # build a Path object, passing it the path to the weather data file we want
 lines = path.read_text().splitlines()   # read the file and chain the splitlines() method to get a list of all the lines in the file
 
 reader = csv.reader(lines) # create a reader object using csv.reader() and pass it the list of lines from the csv file
 header_row = next(reader) # gets the first line of the file, which contains the file headers
 
+# automatic indexes (for reading various csv data files)
+for index, column_header in enumerate(header_row):
+    match column_header.strip():
+        case "DATE":
+            date = index
+        case "TMAX":
+            tmax = index
+        case "TMIN":
+            tmin = index
+        case _:
+            continue
+
 # extract the dates and high/low temperatures
 dates, highs, lows = [], [], []
 # since we already read the header row, the loop will begin at the second line where the actual data begins
 for row in reader: 
-    current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[4]) # on each pass through the loop we pull the data from index 4, corresponding to the column TMAX
-    low = int(row[5]) # TMIN
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    current_date = datetime.strptime(row[date], '%Y-%m-%d')
+    try:
+        high = int(row[tmax])
+        low = int(row[tmin]) 
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 # plot the high and low temperatures
 plt.style.use('ggplot')
@@ -29,7 +45,8 @@ ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # formal plot
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel('Temperature (F)', fontsize=16)
